96a6f6d3f98f3eaefe48fe474081c93c5336a559
gorkemtopal <gorkeem.topal@gmail.com>
2024-06-12 13:15:52 +0300

    personel yazıldı

---
+++ Personel.py
@@ -0,0 +1,45 @@
+# Personel.py
+class Personel:
+    def __init__(self, personel_no, ad, soyad, departman, maas):
+        # Private değişkenler tanımlanıyor
+        self.__personel_no = personel_no
+        self.__ad = ad
+        self.__soyad = soyad
+        self.__departman = departman
+        self.__maas = maas
+
+    # Getter metotları
+    def get_personel_no(self):
+        return self.__personel_no
+
+    def get_ad(self):
+        return self.__ad
+
+    def get_soyad(self):
+        return self.__soyad
+
+    def get_departman(self):
+        return self.__departman
+
+    def get_maas(self):
+        return self.__maas
+
+    # Setter metotları
+    def set_personel_no(self, personel_no):
+        self.__personel_no = personel_no
+
+    def set_ad(self, ad):
+        self.__ad = ad
+
+    def set_soyad(self, soyad):
+        self.__soyad = soyad
+
+    def set_departman(self, departman):
+        self.__departman = departman
+
+    def set_maas(self, maas):
+        self.__maas = maas
+
+    # Personel bilgilerini döndüren özel __str__ metodu
+    def __str__(self):
+        return f"Personel No: {self.__personel_no}, Ad: {self.__ad}, Soyad: {self.__soyad}, Departman: {self.__departman}, Maaş: {self.__maas}"
